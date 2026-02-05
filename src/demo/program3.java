0e6ee0d3628b6d367cb94adee4c9d4b4de3af3b0
mounikasree004-collab <mouni@Mounika.lan>
2026-02-05 08:57:19 +0530

    my first commit

--- src/demo/program3.java
+++ src/demo/program3.java
@@ -1,5 +1,16 @@
 package demo;
 
-public class program3 {
+import java.util.ArrayList;
 
+public class program3 {
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		al.add(100);
+		al.add(200);
+		al.add(300);
+		al.add(400);
+		al.add(500);
+		System.out.println(al);
+		
+	}
 }
