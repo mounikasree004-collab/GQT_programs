103646d84365f2eadb8721276a50dcb9c772db77
mounikasree004-collab <mounikasree004@gmail.com>
2026-02-05 08:42:25 +0530

    Duplicate 'Hello World' print statements

--- src/demo/program1.java
+++ src/demo/program1.java
@@ -3,8 +3,9 @@ package demo;
 public class program1 {
 
 	public static void main(String[] args) {
-	System.out.println("Hello World");
-
+	    System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 
 }
